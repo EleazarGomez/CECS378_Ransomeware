e7fbfd4829f5b08fbd0074d383b3cf00723af5eb
Eli <eliaaron.gomez@gmail.com>
2018-10-11 15:54:58 -0700

    Updated Myencrypt.py

--- File_Encryption/Myencrypt.py
+++ File_Encryption/Myencrypt.py
@@ -1,75 +1,52 @@
 import os
-#import sys
-#import json
-#import base64
 
 from CONSTANTS import *
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
 
-def Myencrypt(plaintext, key):
+#from pathlib import Path
+
+def Myencrypt(message, key):
     if (len(key) * BITS_PER_BYTE != KEY_LENGTH_BITS):
         raise ValueError("Incorrect key length. Must be 256 bits.")
-    
+
     IV = os.urandom(IV_LENGTH_BYTES)
+    padder = padding.PKCS7(MESSAGE_LENGTH_BITS).padder()
+	
+    message = padder.update(message)
+    message += padder.finalize()
+	
+    encryptor = Cipher(algorithms.AES(key), modes.CBC(IV),
+                backend = default_backend()).encryptor()
+    C = encryptor.update(message) + encryptor.finalize()
+	
+    return (C, IV)
 
-    encryptor = Cipher(algorithms.AES(key), modes.GCM(iv),
-                       backend = default_backend()).encryptor()
-
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-
-    return (ciphertext, IV)
+def Mydecrypt(ciphertext, key, IV):
+    decryptor = Cipher(algorithms.AES(key), modes.CBC(IV),
+                       backend = default_backend()).decryptor()
     
-
-def Mydecrypt(ciphertext, IV, key):
-  decryptor = Cipher(algorithms.AES(key), modes.GCM(iv, tag),
-                     backend=default_backend()).decryptor()
-
-  return decryptor.update(ciphertext) + decryptor.finalize()
+    message = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(MESSAGE_LENGTH_BITS).unpadder()
+    message = unpadder.update(message)
+    message = message + unpadder.finalize()
+    
+    return message
 
 # =====================
 # TEST
 # =====================
 
-test_key = os.urandom(KEY_LENGTH_BITS / BITS_PER_BYTE)
+test_key = os.urandom(int(KEY_LENGTH_BITS / BITS_PER_BYTE))
 
-print(Myencrypt("test plaintext", test_key))
+msg = "test plaintext"
 
+msg_bytes = str.encode(msg)
 
-##def MyfileEncrypt(filepath):
-##  key_length = 32
-##  key = os.urandom(key_length)
-##  
-##  file_name = os.path.splitext(filepath)[0]
-##  file_extension = os.path.splitext(filepath)[1]
-##
-##  with open(filepath, "rb") as binary_file:
-##    # Read the whole file at once
-##    data = binary_file.read()
-##    iv, ciphertext, tag = Myencrypt(
-##      data,
-##      key
-##    )
-##    
-##    return (ciphertext, tag, iv, key, file_extension)
-##
-##def MyfileDecrypt(filepath, key):
-##  file_name = os.path.splitext(filepath)[0]
-##  
-##  with open(filepath, 'r') as f:
-##    data = json.load(f)
-##    
-##    iv = base64.b64decode(data['iv'])
-##    ciphertext = base64.b64decode(data['ciphertext_base64'])
-##    tag = base64.b64decode(data['tag'])
-##  
-##    plaintext = Mydecrypt(ciphertext, tag, iv, key)
-##    
-##    output_filename = file_name + data['file_extension']
-##    
-##    f = open(output_filename, 'wb')
-##    f.write(plaintext)
-##    f.close()
-##    
-##    return output_filename
+print(msg_bytes)
+
+x = Myencrypt(msg_bytes, test_key)
+
+print(Mydecrypt(x[0], test_key, x[1]))
