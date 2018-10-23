e98f3fdacb4e50a61fe88b4cfcf3f2e6164ea95d
Eli <eliaaron.gomez@gmail.com>
2018-10-23 15:04:40 -0700

    modified MyfileEncryptMac, test case working

--- File_Encryption/MyfileEncryptMAC.py
+++ File_Encryption/MyfileEncryptMAC.py
@@ -1,13 +1,16 @@
 import os
+import json
 
 from CONSTANTS import *
-from Myencrypt import *
+from MyencryptMAC import *
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding, serialization, hashes, hmac
+from cryptography.hazmat.primitives import padding, hashes, hmac
+
 
 def MyfileEncryptMAC(filepath):
+    # Read file
     splitFilePath = os.path.splitext(filepath)
     ext = splitFilePath[1]
     
@@ -15,38 +18,50 @@ def MyfileEncryptMAC(filepath):
         file = open(filepath, 'r')
         content = file.read()
         encodedContent = content.encode()
-        encodedFilepath = '..\\..\\encodedFileMAC.txt'
+        encodedFilepath = '..\\..\\encodedFile.txt'
     else:
         file = open(filepath, 'rb')
         encodedContent = file.read()
-        encodedFilepath = '..\\..\\encodedFileMAC.jpg'
+        encodedFilepath = '..\\..\\encodedFile.jpeg'
         
     file.close()
 
-    EncKey = os.urandom(32)
-    HMACKey = os.urandom(32)
+    # Generate keys
+    EncKey = os.urandom(KEY_LENGTH_BYTES)
+    HMACKey = os.urandom(KEY_LENGTH_BYTES)
 
+    # Encrypt
     C, IV, tag = MyencryptMAC(encodedContent, EncKey, HMACKey)
+    
     file = open(encodedFilepath, 'wb')
     file.write(C)
     file.close()
+    
+    return C, IV, EncKey, HMACKey, tag, ext, encodedFilepath
 
-    return C, IV, tag, EncKey, HMACKey, ext, encodedFilepath
-
-def MyfileDecryptMAC(filepath, C, IV, tag, Enckey, HMACKey, ext):
+def MyfileDecryptMAC(IV, EncKey, HMACKey, tag, ext, filepath):
+    # Read file
     file = open(filepath, 'rb')
     content = file.read()
     file.close()
-    
-    message = MydecryptMAC(content, IV, EncKey, tag, HMACKey)
-    
+
+    # Verify tag
+    verificationTag = hmac.HMAC(HMACKey, hashes.SHA256(),
+                               backend = default_backend())
+    verificationTag.update(content)
+    verificationTag.verify(tag)
+
+    # Decrypt
+    message = MydecryptMAC(content, IV, tag, EncKey, HMACKey)
+
+    # Output
     if(ext == '.txt'):
         message = message.decode()
-        file = open('..\\..\\decodedFileMAC.txt', 'w')
+        file = open('..\\..\\decodedFile.txt', 'w')
         file.write(message)
         file.close()
     else:
-        file = open('..\\..\\decodedFileMAC.jpg', 'wb')
+        file = open('..\\..\\decodedFile.jpeg', 'wb')
         file.write(message)
         file.close()
             
@@ -57,14 +72,14 @@ def MyfileDecryptMAC(filepath, C, IV, tag, Enckey, HMACKey, ext):
 # =====================
 
 if __name__ == "__main__":
-    print("Which file do you wish to test, 1. test.txt or 2. test_image.jpg? ")
+    print("Which file do you wish to test, 1. TXT_test.txt or 2. JPEG_test.jpeg? ")
     choice = 0
 
     while (choice != 1 and choice != 2):
         try:
             choice = int(input())
             if (choice != 1 and choice != 2):
-                print("Invalid choice")
+                print("Invalide choice")
             else:
                 break
         except:
@@ -72,8 +87,8 @@ if __name__ == "__main__":
 
     if (choice == 1):
         x = MyfileEncryptMAC("..\\Test_Files\\TXT_test.txt")
-        message = MyfileDecryptMAC(x[4], x[2], x[1], x[3])
+        message = MyfileDecryptMAC(x[1], x[2], x[3], x[4], x[5], x[6])
         print(message)
     else:
-        x = MyfileEncryptMAC("..\\Test_Files\\JPG_test.jpg")
-        message = MyfileDecryptMAC(x[4], x[2], x[1], x[3])
+        x = MyfileEncryptMAC("..\\Test_Files\\JPEG_test.jpeg")
+        message = MyfileDecryptMAC(x[1], x[2], x[3], x[4], x[5], x[6])
